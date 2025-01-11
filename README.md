0d11ed02ed3b9b6b09cbffa4d27e6057163482e6
Nagaraj <nagarajnainar8458@gmail.com>
2025-01-11 12:36:49 +0530

    route structure

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ provide a movie api for reactjs app
 
 ### important commands
 
-1.npm install
+1.`npm install`
 2.`npm run server`
 3.`git init`
 4.`git add .`
--- main.js
+++ main.js
@@ -8,6 +8,30 @@ app.get('/', (req, res) => {
     res.json({msg: "hello World"})
 })
 
+// CRUD functionality of movies
+
+// R - for reading movies
+app.get('/movies', (req, res) => {
+
+
+})
+
+// C - for Creating movies
+app.post('/movies', (req, res) => {
+    
+
+})
+
+// U - for  Updating Movie
+app.put('/movies/:id', () => {
+
+} )
+
+app.delete('/movies/:id', () => {
+
+})
+
+// D - for Deleting the movie
 app.listen(PORT, () => {
     console.log(`the app was running at http://localhost${PORT}`)
 })
